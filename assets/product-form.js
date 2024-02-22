fe8b2a46a29ad36b6c54b7372f8a735c80fd6fff
Markus Mälzer <markus.maelzer@gmail.com>
2024-02-22 14:03:08 +0100

    fix?

--- assets/product-form.js
+++ assets/product-form.js
@@ -8,9 +8,12 @@ if (!customElements.get('product-form')) {
         this.form = this.querySelector('form');
         this.form.querySelector('[name=id]').disabled = false;
         this.form.addEventListener('submit', this.onSubmitHandler.bind(this));
-        this.cart = document.querySelector('cart-notification') || document.querySelector('cart-drawer');
+        this.cart =
+          document.querySelector('cart-notification') ||
+          document.querySelector('cart-drawer');
         this.submitButton = this.querySelector('[type="submit"]');
-        if (document.querySelector('cart-drawer')) this.submitButton.setAttribute('aria-haspopup', 'dialog');
+        if (document.querySelector('cart-drawer'))
+          this.submitButton?.setAttribute('aria-haspopup', 'dialog');
 
         this.hideErrors = this.dataset.hideErrors === 'true';
       }
@@ -21,7 +24,7 @@ if (!customElements.get('product-form')) {
 
         this.handleErrorMessage();
 
-        this.submitButton.setAttribute('aria-disabled', true);
+        this.submitButton?.setAttribute('aria-disabled', true);
         this.submitButton.classList.add('loading');
         this.querySelector('.loading-overlay__spinner').classList.remove('hidden');
 
@@ -48,13 +51,14 @@ if (!customElements.get('product-form')) {
                 source: 'product-form',
                 productVariantId: formData.get('id'),
                 errors: response.errors || response.description,
-                message: response.message
+                message: response.message,
               });
               this.handleErrorMessage(response.description);
 
-              const soldOutMessage = this.submitButton.querySelector('.sold-out-message');
+              const soldOutMessage =
+                this.submitButton.querySelector('.sold-out-message');
               if (!soldOutMessage) return;
-              this.submitButton.setAttribute('aria-disabled', true);
+              this.submitButton?.setAttribute('aria-disabled', true);
               this.submitButton.querySelector('span').classList.add('hidden');
               soldOutMessage.classList.remove('hidden');
               this.error = true;
@@ -65,7 +69,10 @@ if (!customElements.get('product-form')) {
             }
 
             if (!this.error)
-              publish(PUB_SUB_EVENTS.cartUpdate, { source: 'product-form', productVariantId: formData.get('id') });
+              publish(PUB_SUB_EVENTS.cartUpdate, {
+                source: 'product-form',
+                productVariantId: formData.get('id'),
+              });
             this.error = false;
             const quickAddModal = this.closest('quick-add-modal');
             if (quickAddModal) {
@@ -88,7 +95,8 @@ if (!customElements.get('product-form')) {
           })
           .finally(() => {
             this.submitButton.classList.remove('loading');
-            if (this.cart && this.cart.classList.contains('is-empty')) this.cart.classList.remove('is-empty');
+            if (this.cart && this.cart.classList.contains('is-empty'))
+              this.cart.classList.remove('is-empty');
             if (!this.error) this.submitButton.removeAttribute('aria-disabled');
             this.querySelector('.loading-overlay__spinner').classList.add('hidden');
           });
@@ -98,9 +106,12 @@ if (!customElements.get('product-form')) {
         if (this.hideErrors) return;
 
         this.errorMessageWrapper =
-          this.errorMessageWrapper || this.querySelector('.product-form__error-message-wrapper');
+          this.errorMessageWrapper ||
+          this.querySelector('.product-form__error-message-wrapper');
         if (!this.errorMessageWrapper) return;
-        this.errorMessage = this.errorMessage || this.errorMessageWrapper.querySelector('.product-form__error-message');
+        this.errorMessage =
+          this.errorMessage ||
+          this.errorMessageWrapper.querySelector('.product-form__error-message');
 
         this.errorMessageWrapper.toggleAttribute('hidden', !errorMessage);
 
